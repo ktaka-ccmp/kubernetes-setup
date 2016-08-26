ccb4021f2fd72feb9661c69623280e9cb2f84f3f
ktaka-ccmp <ktaka@ccmp.jp>
2016-08-26 21:59:38 +0900

    Revert "Fulltls"

--- master.sh
+++ master.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-. ./env
+MASTER_IP=172.16.1.101
 
 ########## Install docker
 apt-get update
@@ -10,8 +10,21 @@ aptitude -y update
 aptitude -y upgrade
 
 ###########
+CERTDIR=/srv/kubernetes/
 
-./MISC/mkcert.sh
+if [ -d $CERTDIR ] ; then
+        echo $CERTDIR already exist.
+        exit 0
+fi
+
+mkdir -p $CERTDIR && \
+pushd $CERTDIR && \
+openssl genrsa -out ca.key 2048 && \
+openssl req -x509 -new -nodes -key ca.key -subj "/CN=${MASTER_IP}" -days 10000 -out ca.crt && \
+openssl genrsa -out server.key 2048 && \
+openssl req -new -key server.key -subj "/CN=${MASTER_IP}" -out server.csr && \
+openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 10000 && \
+popd
 
 ###########
 rsync -av ./rootfs_common/ /
@@ -19,8 +32,6 @@ rsync -av ./rootfs_master/ /
 gunzip -f /usr/local/bin/hyperkube.gz
 gunzip -f /usr/local/bin/etcd.gz
 gunzip -f /usr/local/bin/etcdctl.gz
-
-for dir in /usr/service/etcd/env ; do echo ${MASTER_IP} > ${dir}/MASTER_IP ; done
 
 aptitude install -y daemontools-run 
 
